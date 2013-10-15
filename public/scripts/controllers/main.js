572ecc55bc2c68298cea313acb9b28a4cc40f722
karlos g liberal <karlosgliberal@gmail.com>
2013-10-15 10:23:03 +0200

    Corrección bug cambio de imagen cabecera

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('bimeApp')
-  .controller('MainCtrl', function ($scope, $location, $translate) {
+  .controller('MainCtrl', function ($scope, $location, $translate, $route) {
+  	$scope.img_translate = '/img/banner-es.png'
   	$scope.changeLanguage = function (langKey) {
     	$translate.uses(langKey);
+    	$scope.img_translate = '/img/banner-'+langKey+'.png';
   	};	
  	$scope.go = function (path) {
      	$location.path(path);
-  	}
-  	$scope.img_translate = $translate('IMG_CABECERA');
-  	$scope.img = $scope.img_translate;
-   
+  }
 });
